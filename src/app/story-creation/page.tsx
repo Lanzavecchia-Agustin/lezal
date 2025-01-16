8080b0476b6e76c963505e7b3480afde6102709e
Facundo Lemo <facundolemo@gmail.com>
2025-01-16 18:52:00 -0300

    json-server installation, db.json, form posts to db

--- src/app/story-creation/page.tsx
+++ src/app/story-creation/page.tsx
@@ -1,38 +1,42 @@
 "use client";
 import React, { Fragment, useState } from "react";
-import { ATRIBUTOS_DISPONIBLES, LOCKED_ATTRIBUTES } from "../../../roomsStore";
+import { ATRIBUTOS_DISPONIBLES, LOCKED_ATTRIBUTES, Scene } from "../../../roomsStore";
 import SceneFormOption from "../../components/story-creation/sceneFormOption";
-import { FormSceneBasicData, FormSceneOptionData } from "@/components/story-creation/InterfacesSceneFormOption";
+import {
+  FormSceneBasicData,
+  FormSceneOptionData,
+} from "@/components/story-creation/InterfacesSceneFormOption";
 
 type AtributosDisponibles = (typeof ATRIBUTOS_DISPONIBLES)[number] | ""; // Esto crea un tipo de unión con los valores de ATRIBUTOS_DISPONIBLES
 type LockedAttributes = (typeof LOCKED_ATTRIBUTES)[number] | ""; // Esto crea un tipo de unión con los valores de LOCKED_Attributes
 
-
 const Page: React.FC = () => {
   const [unlocksLockedAttribute, setUnlocksLockedAttribute] =
     useState<boolean>(false);
-  const [formSceneBasicData, setFormSceneBasicData] = useState<FormSceneBasicData>({
-    id: "",
-    text: "",
-    isEnding: false,
-  });
-  const [formSceneOptionData, setFormSceneOptionData] = useState<FormSceneOptionData>({
-    id: 1,
-    text: "",
-    maxVotes: 0,
-    requirement: [] as AtributosDisponibles[],
-    lockedAttributeIncrement: {
-      attribute: "" as LockedAttributes, // Usa el tipo correcto
-      increment: 0,
-    },
-    nextSceneId: {
-      success: "",
-      failure: "",
-      partial: "",
-    },
-  });
+  const [formSceneBasicData, setFormSceneBasicData] =
+    useState<FormSceneBasicData>({
+      id: "",
+      text: "",
+      isEnding: false,
+    });
+  const [formSceneOptionData, setFormSceneOptionData] =
+    useState<FormSceneOptionData>({
+      id: 1,
+      text: "",
+      maxVotes: 0,
+      requirement: [] as AtributosDisponibles[],
+      lockedAttributeIncrement: {
+        attribute: "" as LockedAttributes, // Usa el tipo correcto
+        increment: 0,
+      },
+      nextSceneId: {
+        success: "",
+        failure: "",
+        partial: "",
+      },
+    });
   const [sceneOptions, setSceneOptions] = useState<any[]>([]);
-  const [newScenes, setNewScenes] = useState<Record<string, any>>({});
+  const [newScenes, setNewScenes] = useState<Array<object>>([]);
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
@@ -50,7 +54,7 @@ const Page: React.FC = () => {
       e.target.selectedOptions,
       (option) => option.value
     ) as AtributosDisponibles[]; // Asegura que los valores sean del tipo correcto
-  
+
     setFormSceneOptionData((prevState) => ({
       ...prevState,
       [field]: value as AtributosDisponibles[], // Actualiza requirement como un arreglo
@@ -67,7 +71,20 @@ const Page: React.FC = () => {
     });
   };
 
-  const handleSubmit = (e: React.FormEvent) => {
+  const fetchAttributes = async (newScene:Scene) => {
+    try {
+      const response = await fetch("http://localhost:3001/scenes", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(newScene),
+      });
+      console.log("agregado con exito")
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
     const newScene = {
       ...formSceneBasicData,
@@ -77,7 +94,7 @@ const Page: React.FC = () => {
     const sceneObject = { [newScene.id]: newScene };
     setNewScenes((prevScenes) => ({
       ...prevScenes,
-      [newScene.id]: newScene, // Añadir la nueva escena como un atributo del objeto
+      ... newScene, 
     }));
     setSceneOptions([]); //borra las opciones que venías creando
     setFormSceneBasicData({
@@ -92,6 +109,7 @@ const Page: React.FC = () => {
     document.getElementById("header")?.scrollIntoView({
       behavior: "smooth",
     });
+    await fetchAttributes(newScene)
   };
 
   return (
@@ -198,7 +216,7 @@ const Page: React.FC = () => {
         <h1>Salida:</h1>
       </div>
       <pre className="max-w-4xl mx-auto p-6 bg-black text-wrap w-2/3 text-white rounded-lg shadow-md">
-        {JSON.stringify(newScenes, null, 2)}
+        {JSON.stringify(newScenes,null, 2)}
       </pre>
     </div>
   );
